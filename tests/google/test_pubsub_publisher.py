6cd274c9385e470ed667d96b4b2a67b4a8a8371a
qchen <qchen@u.circlecat.org>
2025-03-06 00:32:16 +0000

    [PUR-19] Implement workspace subscription function
    Added a new function to create workspace subscription in the system. The function allows users to subscription for a specific workspace.
    
    Change-Id: I35c9d549019a1b49d2dffcaebea8ef4e66bb9d3d

--- tests/google/test_pubsub_publisher.py
+++ tests/google/test_pubsub_publisher.py
@@ -2,11 +2,17 @@ from unittest import TestCase, main
 from unittest.mock import Mock, patch
 import logging
 from io import StringIO
-from google.pubsub_publisher import create_subscription, create_pubsub_topic
+from google.pubsub_publisher import (
+    create_subscription,
+    create_pubsub_topic,
+    create_workspaces_subscriptions,
+)
 
 TEST_PROJECT_ID = "test-project"
 TEST_TOPIC_ID = "test-topic"
 TEST_SUBSCRIPTION_ID = "test-subscription"
+TEST_SPACE_ID = "test-space"
+TEST_EVENT_TYPES = ["google.workspace.chat.message.v1.created"]
 EXPECTED_TOPIC_PATH = f"projects/{TEST_PROJECT_ID}/topics/{TEST_TOPIC_ID}"
 EXPECTED_SUBSCRIPTION_PATH = (
     f"projects/{TEST_PROJECT_ID}/subscriptions/{TEST_SUBSCRIPTION_ID}"
@@ -33,6 +39,38 @@ class TestPubsubPublisher(TestCase):
             TEST_PROJECT_ID, TEST_TOPIC_ID
         )
         mock_publisher.create_topic.assert_called_once_with(name=EXPECTED_TOPIC_PATH)
+
+    @patch("google.pubsub_publisher.GoogleClientFactory")
+    def test_create_workspaces_subscriptions_success(self, mock_client_factory):
+        mock_workspaceevents = Mock()
+        mock_subscriptions = Mock()
+        mock_execute = Mock(return_value={"subscription_id": "test-subscription"})
+
+        mock_subscriptions.create.return_value.execute = mock_execute
+        mock_workspaceevents.subscriptions.return_value = mock_subscriptions
+
+        mock_factory_instance = Mock()
+        mock_factory_instance.create_workspaceevents_client.return_value = (
+            mock_workspaceevents
+        )
+        mock_client_factory.return_value = mock_factory_instance
+
+        response = create_workspaces_subscriptions(
+            TEST_PROJECT_ID, TEST_TOPIC_ID, TEST_SPACE_ID, TEST_EVENT_TYPES
+        )
+
+        expected_body = {
+            "target_resource": f"//chat.googleapis.com/spaces/{TEST_SPACE_ID}",
+            "event_types": TEST_EVENT_TYPES,
+            "notification_endpoint": {
+                "pubsub_topic": f"projects/{TEST_PROJECT_ID}/topics/{TEST_TOPIC_ID}"
+            },
+            "payload_options": {"include_resource": True},
+        }
+
+        mock_subscriptions.create.assert_called_once_with(body=expected_body)
+        mock_execute.assert_called_once()
+        self.assertEqual(response, {"subscription_id": "test-subscription"})
 
     @patch("google.pubsub_publisher.GoogleClientFactory")
     def test_create_subscription_success(self, mock_client_factory):
