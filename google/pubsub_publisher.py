dbeb7fea86fcd778cff61b2c53c712598130130b
qchen <qchen@u.circlecat.org>
2025-03-06 00:32:16 +0000

    [PUR-19] Implement workspace subscription function
    Added a new function to create workspace subscription in the system. The function allows users to subscription for a specific workspace.
    
    Change-Id: I35c9d549019a1b49d2dffcaebea8ef4e66bb9d3d

--- google/pubsub_publisher.py
+++ google/pubsub_publisher.py
@@ -37,6 +37,44 @@ def create_pubsub_topic(project_id, topic_id):
     return topic
 
 
+def create_workspaces_subscriptions(project_id, topic_id, space_id, event_types):
+    """
+    Creates a Google Workspace Events subscription for a specific space.
+
+    Args:
+        project_id (str): The ID of your Google Cloud project.
+        topic_id (str): The Pub/Sub topic ID where events will be published.
+        space_id (str): The ID of the Google Chat space to subscribe to.
+        event_types (list): A list of event types to listen for.
+
+    Returns:
+        dict: The API response containing the subscription details.
+
+    Raises:
+        ValueError: If any of the required parameters are missing.
+    """
+
+    if not project_id or not topic_id or not space_id or not event_types:
+        raise ValueError(
+            "All parameters (project_id, topic_id, space_id, event_types) must be provided."
+        )
+
+    workspaceevents = GoogleClientFactory().create_workspaceevents_client()
+    BODY = {
+        "target_resource": f"//chat.googleapis.com/spaces/{space_id}",
+        "event_types": event_types,
+        "notification_endpoint": {
+            "pubsub_topic": f"projects/{project_id}/topics/{topic_id}"
+        },
+        "payload_options": {"include_resource": True},
+    }
+    response = workspaceevents.subscriptions().create(body=BODY).execute()
+    logging.info(
+        f"Creating subscription for space {space_id} with event types {event_types} on topic {topic_id}. Response: {response}"
+    )
+    return response
+
+
 def create_subscription(project_id, topic_id, subscription_id):
     """
     Creates a Google Cloud Pub/Sub subscription for a given topic with no expiration.
