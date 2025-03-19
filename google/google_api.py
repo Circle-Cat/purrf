f1247902b519a96e717e41b85e080d6df1ce2429
Qian Chen <qchen@u.circlecat.org>
2025-03-19 07:00:28 +0000

    [PUR-30] Implement get chat spaces route
    Added a new route `/api/chat/spaces` to the application. API endpoint to retrieve chat spaces of a specified type and page size.
    
    Change-Id: Iea444ac3ddb0ed10b527954ac33af53eb7c383da

--- google/google_api.py
+++ google/google_api.py
@@ -1,14 +1,14 @@
 """google chat service"""
 
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from google.fetch_history_chat_message import fetch_history_messages
+from google.chat_utils import get_chat_spaces
 import http.client
 from concurrent.futures import ThreadPoolExecutor
 
 google_bp = Blueprint("google", __name__)
 
 executor = ThreadPoolExecutor(max_workers=5)
-
 
 @google_bp.route("/api/chat/spaces/messages")
 def history_messages():
@@ -18,3 +18,13 @@ def history_messages():
     return jsonify({
         "message": "Message retrieval triggered asynchronously."
     }), http.client.ACCEPTED
+
+@google_bp.route("/api/chat/spaces")
+def get_chat_spaces_route():
+    """API endpoint to retrieve chat spaces of a specified type and page size."""
+
+    space_type = request.args.get("space_type", "SPACE")
+    page_size = int(request.args.get("page_size", 100))
+
+    spaces = get_chat_spaces(space_type, page_size)
+    return jsonify({"spaces": spaces}), 200
