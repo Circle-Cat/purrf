9955a11db032e97fca945851e4d62ded26bed23b
Yanpei Wang <yuji@u.circlecat.org>
2025-05-03 10:50:32 +0000

    [PUR-59] Refactor move Google API client creation to common module and update credential handling
    - Moved client creation logic to `src/common/google_client.py`
    - Replaced impersonation-based credentials with support for `compute_engine.Credentials` due to GKE deployment environment
    
    Change-Id: If01539ca569a290413662ff24380d6d34bba970a

---
+++ src/common/constants.py
@@ -0,0 +1,6 @@
+GOOGLE_SCOPES_LIST = [
+    "https://www.googleapis.com/auth/directory.readonly",
+    "https://www.googleapis.com/auth/chat.spaces.readonly",
+    "https://www.googleapis.com/auth/chat.memberships.readonly",
+    "https://www.googleapis.com/auth/chat.messages.readonly",
+]
