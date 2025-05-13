29f754e9e8260176a5277931a64e5559169eb9d6
Yanpei Wang <yuji@u.circlecat.org>
2025-05-13 08:40:13 +0000

    [PUR-79] Integrate Microsoft Graph API to fetch users and save LDAPS to Redis
    - Instantiate Microsoft Graph API client
    - Always create a new client instance to avoid event loop conflicts:
      - Async clients bind to the event loop at creation.
      - Reusing them across loops can cause "event loop is closed" error.
    - Fetch user mail, account status, and display name
    - Extract LDAP from email prefix
    - Store data into Redis with hash maps per account status:
      - HSET ldap:{account_status} <ldap> "<display_name>"
    - Add HTTP POST endpoint to trigger the fetching and saving process:
      - /api/microsoft/backfill/ldaps
    - Perform incremental updates: compare current Redis state and only update changed or new records
    
    Change-Id: I2a3485127c9a51e1b3ed653f212a9e256a2b5ea4

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 GOOGLE_SCOPES_LIST = [
     "https://www.googleapis.com/auth/directory.readonly",
     "https://www.googleapis.com/auth/chat.spaces.readonly",
@@ -6,6 +8,7 @@ GOOGLE_SCOPES_LIST = [
     "https://www.googleapis.com/auth/calendar.readonly",
 ]
 
+MICROSOFT_SCOPES_LIST = ["https://graph.microsoft.com/.default"]
 
 GOOGLE_CHAT_EVENT_TYPE_CREATE = "created"
 GOOGLE_CHAT_EVENT_TYPE_UPDATE = "updated"
@@ -26,6 +29,31 @@ VALUE = "value"
 MESSAGE_THREAD_ID = "threadId"
 MESSAGE_ATTACHMENT = "attachment"
 
-# Constants for Redis keys and members
+# Constants for Redis keys
 CREATED_GOOGLE_CHAT_MESSAGES_INDEX_KEY = "google:chat:created:{sender_ldap}:{space_id}"
 DELETED_GOOGLE_CHAT_MESSAGES_INDEX_KEY = "google:chat:deleted:{sender_ldap}:{space_id}"
+
+MICROSOFT_LDAP_KEY = "ldap:{account_status}"
+
+
+class MicrosoftAccountStatus(str, Enum):
+    ACTIVE = "active"
+    TERMINATED = "terminated"
+    ALL = "all"
+
+    @classmethod
+    def validate_status(cls, status: str) -> "MicrosoftAccountStatus":
+        """Validate the input status to check if it is valid, and return the corresponding enum value."""
+        try:
+            return cls(status.lower())
+        except ValueError:
+            valid_statuses = [e.value for e in cls]
+            raise ValueError(
+                f"Invalid status. Valid values are: {', '.join(valid_statuses)}"
+            )
+
+
+MICROSOFT_USER_INFO_FILTER = "endswith(mail,'circlecat.org')"
+MICROSOFT_USER_INFO_SELECT_FIELDS = ["displayName", "mail", "accountEnabled"]
+MICROSOFT_CONSISTENCY_HEADER = "ConsistencyLevel"
+MICROSOFT_CONSISTENCY_VALUE = "eventual"
