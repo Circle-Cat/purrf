bba0745eedb4811f3d117e1047029eb2253e6981
Yanpei Wang <yuji@u.circlecat.org>
2025-05-13 09:24:23 +0000

    [PUR-59] Migrate redis client factory
    Change-Id: Ie905e6f32e31ad3970a8111bc3e57ad25c51dd7d

--- src/common/constants.py
+++ src/common/constants.py
@@ -4,3 +4,27 @@ GOOGLE_SCOPES_LIST = [
     "https://www.googleapis.com/auth/chat.memberships.readonly",
     "https://www.googleapis.com/auth/chat.messages.readonly",
 ]
+
+
+GOOGLE_CHAT_EVENT_TYPE_CREATE = "created"
+GOOGLE_CHAT_EVENT_TYPE_UPDATE = "updated"
+GOOGLE_CHAT_EVENT_TYPE_DELETE = "deleted"
+
+# Constants for google chat message field names
+MESSAGE = "message"
+NAME = "name"
+MESSAGE_SENDER = "sender"
+CREATE_TIME = "createTime"
+MESSAGE_TEXT = "text"
+MESSAGE_THREAD = "thread"
+MESSAGE_SPACE = "space"
+MESSAGE_LAST_UPDATE_TIME = "lastUpdateTime"
+MESSAGE_DELETION_METADATA = "deletionMetadata"
+MESSAGE_DELETION_TYPE = "deletionType"
+VALUE = "value"
+MESSAGE_THREAD_ID = "threadId"
+MESSAGE_ATTACHMENT = "attachment"
+
+# Constants for Redis keys and members
+CREATED_GOOGLE_CHAT_MESSAGES_INDEX_KEY = "google:chat:created:{sender_ldap}:{space_id}"
+DELETED_GOOGLE_CHAT_MESSAGES_INDEX_KEY = "google:chat:deleted:{sender_ldap}:{space_id}"
