789dd67d09b4f4bd68adcbbf82400b208cc034e6
Qian Chen <qchen@u.circlecat.org>
2025-12-09 00:20:59 +0000

    [PUR-271] Implement get_training_by_user_id
    - Define the required data models: TrainingEntity
    
    - Implement async def get_training_by_user_id
    
    - Add unit tests
    
    Change-Id: I2d4b88ac9eee3c99ecd0fab65d358df4d193bc9a
    Reviewed-on: https://review.circlecat.org/c/purrf/+/8768
    LGTM: Yanpei Wang <yuji@u.circlecat.org>
    Approval: Yanpei Wang <yuji@u.circlecat.org>
    Approval: Jasmine Wang <jasmine@u.circlecat.org>
    Tested-by: Presubmit <CatBot@u.circlecat.org>
    LGTM: Jasmine Wang <jasmine@u.circlecat.org>

--- backend/common/mentorship_enums.py
+++ backend/common/mentorship_enums.py
@@ -6,3 +6,16 @@ class UserTimezone(Enum):
     America_New_York = "America/New_York"
     Asia_Shanghai = "Asia/Shanghai"
     America_Denver = "America/Denver"
+
+
+class TrainingCategory(str, Enum):
+    MENTORSHIP_MENTEE_ONBOARDING = "mentorship_mentee_onboarding"
+    MENTORSHIP_MENTOR_ONBOARDING = "mentorship_mentor_onboarding"
+    RESIDENCY_PROGRAM_ONBOARDING = "residency_program_onboarding"
+    CORPORATE_CULTURE_COURSE = "corporate_culture_course"
+
+
+class TrainingStatus(str, Enum):
+    TO_DO = "to_do"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
--- backend/entity/training_entity.py
+++ backend/entity/training_entity.py
@@ -1,2 +1,42 @@
-class TrainingEntity:
-    pass
+from typing import Optional
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Enum, TIMESTAMP, ForeignKey
+from backend.common.mentorship_enums import TrainingStatus, TrainingCategory
+from backend.common.base import Base
+
+
+class TrainingEntity(Base):
+    __tablename__ = "training"
+
+    training_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.user_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    category: Mapped[TrainingCategory] = mapped_column(
+        Enum(TrainingCategory, native_enum=False),
+        nullable=False,
+    )
+
+    completed_timestamp: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        nullable=False,
+    )
+
+    status: Mapped[TrainingStatus] = mapped_column(
+        Enum(TrainingStatus, native_enum=False),
+        nullable=False,
+    )
+
+    deadline: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        nullable=True,
+    )
+
+    link: Mapped[Optional[str]] = mapped_column(
+        String,
+        nullable=True,
+    )
