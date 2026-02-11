d61264f6cca0bfed1c45a38a20703e43858c6ecd
Yuanyuan Huang <yhuang@u.circlecat.org>
2026-02-11 03:01:43 +0000

    [PUR-387] Deploy 20260211 to test environment
    Change-Id: I8c4ab54a132e99bf143476c75d7bc49d321fdac6
    Reviewed-on: https://review.circlecat.org/c/purrf/+/9169
    Approval: Yanpei Wang <yuji@u.circlecat.org>
    Tested-by: Presubmit <CatBot@u.circlecat.org>
    LGTM: Yanpei Wang <yuji@u.circlecat.org>

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -21,7 +21,7 @@ module "purrf_instance" {
   gerrit_producer_entry_point = "gerrit_event_webhook"
   jira_password               = var.jira_password
   gerrit_http_pass            = var.gerrit_http_pass
-  image_tag                   = "20260210-021844"
+  image_tag                   = "20260211-021832"
   cf_aud_tag                  = "8187a0acb20eed9fe7b5d2290977c85490f6c7f3844595f521dc6f3328ca37d0"
   azure_client_id             = "8f3f85f2-be71-4ed5-95e8-3c777f4c6e13"
   azure_tenant_id             = "08502fd6-503a-4dfd-85b7-f13b141dc0c4"
