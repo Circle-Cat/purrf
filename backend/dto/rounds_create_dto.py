a895b45c369e56c849a6082b0608ba1315b08cb0
Yuanyuan Huang <yhuang@u.circlecat.org>
2026-05-20 09:15:30 +0000

    [PUR-457] Update timeline fields in TimelineCreateDto
    Make non-essential TimelineCreateDto fields optional and add new
    timeline fields for Mentorship Admin Round Management.
    
    Change-Id: Ie56e73c108619b6582b6c4eaf6dfa3e58b22eb92
    Reviewed-on: https://review.circlecat.org/c/purrf/+/9684
    Tested-by: Presubmit <CatBot@u.circlecat.org>
    LGTM: Yanpei Wang <yuji@u.circlecat.org>
    LGTM: Jasmine Wang <jasmine@u.circlecat.org>
    Approval: Jasmine Wang <jasmine@u.circlecat.org>
    (cherry picked from commit 17aaa4abf621abfa026c7be5eb904ab5b6ab272f)

--- backend/dto/rounds_create_dto.py
+++ backend/dto/rounds_create_dto.py
@@ -6,13 +6,17 @@ class TimelineCreateDto(BaseRequestDto):
     promotion_start_at: datetime
     mentor_application_deadline_at: datetime
     mentee_application_deadline_at: datetime
-    review_start_at: datetime
-    acceptance_notification_at: datetime
-    matching_completed_at: datetime
+    training_notification_at: datetime | None = None
+    training_deadline_at: datetime | None = None
     match_notification_at: datetime
-    first_meeting_deadline_at: datetime
+    matching_completed_at: datetime | None = None
+    meeting_log_reminder_at: datetime | None = None
     meetings_completion_deadline_at: datetime
-    feedback_deadline_at: datetime
+    feedback_start_at: datetime | None = None
+    feedback_deadline_at: datetime | None = None
+    review_start_at: datetime | None = None
+    acceptance_notification_at: datetime | None = None
+    first_meeting_deadline_at: datetime | None = None
 
 
 class RoundsCreateDto(BaseRequestDto):
